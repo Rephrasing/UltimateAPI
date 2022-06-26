c3fc6dc360e1390e84d3404962049adb77d1362f
Rephrasing <iamnotifrankieee8282@gmail.com>
2022-06-26 19:58:28 +0200

    minor details

--- src/main/java/com/github/rephrasing/ultimateapi/guis/SubGUI.java
+++ src/main/java/com/github/rephrasing/ultimateapi/guis/SubGUI.java
@@ -5,12 +5,20 @@ import org.bukkit.entity.Player;
 
 public class SubGUI extends AbstractGUI {
 
-    public SubGUI(Player player, int size) {
+    private final GUI mainGUI;
+
+    public SubGUI(Player player, int size, GUI mainGUI) {
         super(player, size);
+        this.mainGUI = mainGUI;
     }
 
-    public SubGUI(Player player, String name, int size) {
+    public SubGUI(Player player, String name, int size, GUI mainGUI) {
         super(player, name, size);
+        this.mainGUI = mainGUI;
     }
 
+
+    public GUI getMainGUI() {
+        return mainGUI;
+    }
 }
