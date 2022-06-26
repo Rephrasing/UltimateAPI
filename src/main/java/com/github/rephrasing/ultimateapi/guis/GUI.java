821f78f538ec853ed21a18d197c5026dcea72437
Rephrasing <iamnotifrankieee8282@gmail.com>
2022-06-26 21:05:08 +0200

    adding option for no-subguis GUI

--- src/main/java/com/github/rephrasing/ultimateapi/guis/GUI.java
+++ src/main/java/com/github/rephrasing/ultimateapi/guis/GUI.java
@@ -2,6 +2,8 @@ package com.github.rephrasing.ultimateapi.guis;
 
 import lombok.Getter;
 import org.bukkit.entity.Player;
+
+import java.util.ArrayList;
 import java.util.List;
 
 @Getter
@@ -16,6 +18,17 @@ public class GUI extends AbstractGUI {
     public GUI(Player player, String name, int size, List<SubGUI> subGUIs) {
         super(player, name, size);
         this.subGUIs = subGUIs;
+    }
+
+
+    public GUI(Player player, int size) {
+        super(player, size);
+        subGUIs = new ArrayList<>();
+    }
+
+    public GUI(Player player, String name, int size) {
+        super(player, name, size);
+        subGUIs = new ArrayList<>();
     }
 
 }
